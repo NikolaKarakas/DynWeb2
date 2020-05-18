706d031ebe50cd859a87be85575bdd51faae967d
Nikola Karakas <nikolakarakas95@gmail.com>
2020-05-18 20:50:26 +0200

    m

---
+++ src/com/example/tutorial/Adressa.java
@@ -0,0 +1,13 @@
+package com.example.tutorial;
+
+public class Adressa {
+	private String urlString;
+
+	public String getUrlString() {
+		return urlString;
+	}
+
+	public void setUrlString(String urlString) {
+		this.urlString = urlString;
+	}
+}
